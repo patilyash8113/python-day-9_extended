817e3727694c9b354ed4c49653ed675c20a06d63
patilyash8113 <yashpatil8113@gmail.com>
2025-12-31 12:17:01 +0530

    Add files via upload

---
+++ 04_match_case.py
@@ -0,0 +1,82 @@
+# a=int(input("Enter a lucky number between 1 and 10:"))
+
+# match a:
+#     case 1:
+#         print("you won a cameraa")
+# match a:
+#     case 6:
+#         print("you won a $3")
+# match a:
+#     case 3:
+#         print("you won $1")
+#     case _:
+#         print("Better luck next time")
+
+# a=int(input("Enter the first number:"))
+# b=int(input("Enter the second number:"))
+
+# op=input("Enter operation(+,-,*,/):")
+
+# match op:
+#     case "+":
+#         print("Result:",a+b)
+#     case "-":
+#         print("Result:",a-b)
+#     case "*":
+#         print("Result:",a*b)
+#     case "/":
+#         if b !=0:
+#             print("Result",a/b)
+#         else:
+#             print("cannot divide by zero")
+#     case _:
+#         print("Invalid operation")
+
+
+# a=int(input("Enter the first number:"))
+# b=int(input("Enter the second number:"))
+
+# op=input("Enter operation(+,-,*,/):")
+
+
+# match op:
+#     case "+":
+#         print("Result",a+b)
+#     case "-":
+#         print("Result",a-b)
+#     case "*":
+#         print("Result",a*b)
+#     case "/":
+#        if b !=0:
+#            print("Result",a/b)
+#        else:
+#            print("cannot divide by zero")
+    # case _:
+        # print("Operation is invalid")
+
+
+# print("your welcome!")
+
+
+# role=input("Enter role: ")
+
+# match role:
+#     case "admin":
+#         print("Full access granted!")
+#     case "user":
+#         print("Limited access granted!")
+#     case "guest":
+#         print("Read-only access granted!")
+#     case _:
+#         print("Invalid role")
+
+
+
+
+
+
+
+
+
+
+
